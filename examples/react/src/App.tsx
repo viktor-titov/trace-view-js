7684a9025a9ef9fad83555b2a8290878a2a631e6
Viktor Titov <nulltomato@gmail.com>
2024-05-27 14:01:40 +0300

    change example react

--- examples/react/src/App.tsx
+++ examples/react/src/App.tsx
@@ -1,10 +1,13 @@
 import React, { useEffect, useRef, FC } from 'react'
+import {TraceViewTest} from '@viktor-titov/trace-view-js'
 
 export const App: FC = () => {
   const wrapperRef = useRef(null);
 
   useEffect(() => {
-    // grid.render(wrapperRef.current);
+    if (wrapperRef.current) {
+      TraceViewTest(wrapperRef.current)
+    }
   });
   
   return (
